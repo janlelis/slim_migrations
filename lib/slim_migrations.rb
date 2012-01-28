53c452854a97dfdf0171b8ff8664e6ed5f9ec36d
Manuel Meurer <manuel.meurer@gmail.com>
2012-01-28 16:38:13 +0100

    Fix regex that creates migration class
    Previously the regex would be incorrect if the migration name included a digit and a underscore, e.g. 'add_foo1_to_products'.

--- lib/slim_migrations.rb
+++ lib/slim_migrations.rb
@@ -13,7 +13,7 @@ module Kernel
 
   # initialize migrations with <tt>migration do</tt> instead of <tt>class SomeMigration < ActiveRecord::Migration</tt>
   def migration(&block)
-    if caller[0].rindex(/(?:[0-9]+)_([_a-z0-9]*).rb:\d+(?::in `.*')?$/)
+    if caller[0].rindex(/\/(?:[0-9]+)_([_a-z0-9]*).rb:\d+(?::in `.*')?$/)
       m = Object.const_set $1.camelize, Class.new(ActiveRecord::Migration)
       m.instance_eval(&block) # 3.0
     else
